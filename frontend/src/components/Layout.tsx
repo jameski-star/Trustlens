2667704a5d2306874150e5951f20058a68d19d6f
Chacha <chacha@kali.kali>
2026-06-28 18:48:05 +0300

    CSE

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -6,7 +6,7 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-      <main className="flex-1">
+      <main className="flex-auto">
         {children}
       </main>
       <Footer />
